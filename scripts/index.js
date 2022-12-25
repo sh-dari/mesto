acbdc820a3269563959d39520c98b761ea15aef5
Дарья Шипилова <sh.dari@list.ru>
2022-12-25 16:02:38 +0300

    fix: корректный шрифт

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 let popup = document.querySelector('.popup');
-let formElement = popup.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__item_el_name');
-let jobInput = formElement.querySelector('.popup__item_el_job');
+let popupContainer = popup.querySelector('.popup__container');
+let nameInput = popupContainer.querySelector('.popup__item_el_name');
+let jobInput = popupContainer.querySelector('.popup__item_el_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-let closeButton = formElement.querySelector('.popup__close');
+let closeButton = popupContainer.querySelector('.popup__close');
 let editButton = document.querySelector('.profile__edit-button');
 
 function closePopup() {
@@ -32,4 +32,4 @@ function handleFormSubmit (evt) {
     closePopup();
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+popupContainer.addEventListener('submit', handleFormSubmit);
