b0ae6d5f0e0f698fd303cf6db645e4f434a00b29
Дарья Шипилова <sh.dari@list.ru>
2023-03-11 02:03:55 +0300

    fix: index.js to pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import Card from '../components/Card.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import Section from '../components/Section.js';
-import UserInfo from '../components/UserInfo.js';
-import FormValidator from '../components/FormValidator.js';
+import Card from '../scripts/components/Card.js';
+import PopupWithForm from '../scripts/components/PopupWithForm.js';
+import PopupWithImage from '../scripts/components/PopupWithImage.js';
+import Section from '../scripts/components/Section.js';
+import UserInfo from '../scripts/components/UserInfo.js';
+import FormValidator from '../scripts/components/FormValidator.js';
 import {
   initialCards,
   validationConfig,
@@ -15,8 +15,8 @@ import {
   linkInput,
   buttonEdit,
   buttonAdd
-} from '../utils/constants.js';
-import '../../pages/index.css';
+} from '../scripts/utils/constants.js';
+import './index.css';
 
 const profileValidator = new FormValidator(validationConfig, profilePopupContainer);
 const cardValidator = new FormValidator(validationConfig, cardPopupContainer);
